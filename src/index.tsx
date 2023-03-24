c1b4dd5b52375c32ce65bbaede5b766fe8c0d981
maxpro <maxim.procopenco@cronocode.com>
2023-03-24 17:06:26 +0200

    Add node version into package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import data from './data.json';
+import './index.css';
 
 export default function Map() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" height="90%" viewBox="0 0 1010 666">
-      <g>
-        {data.map((item) => (
-          <path key={item.code} d={item.d} fill="#f6f6f6" stroke="#777" strokeWidth={0.2} />
-        ))}
-      </g>
-    </svg>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" height="90%" viewBox="0 0 1010 666">
+        <g>
+          {data.map((item) => (
+            <path className="country" key={item.code} d={item.d} fill="#f6f6f6" stroke="#777" strokeWidth={0.2} />
+          ))}
+        </g>
+      </svg>
+    </div>
   );
 }
